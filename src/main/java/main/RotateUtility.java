ddfeede28dd133990c7ca5d5675bd8c5d702ecb8
sasha <sashazemzer@gmail.com>
2020-03-12 15:11:33 +0300

    init

---
+++ src/main/java/main/RotateUtility.java
@@ -0,0 +1,12 @@
+package main;
+
+public class RotateUtility {
+
+    public static int rotLeft(int val, int pas) {
+        return (val << pas) | (val >>> (32 - pas));
+    }
+
+    public static int rotRight(int val, int pas) {
+        return (val >>> pas) | (val << (32 - pas));
+    }
+}
